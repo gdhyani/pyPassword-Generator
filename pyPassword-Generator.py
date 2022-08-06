65686f7da56071543effb27d47d163e6bd9e0cfc
gdhyani <gauravdhyani90@gmail.com>
2022-08-06 12:28:50 +0530

    final

--- pyPassword-Generator.py
+++ pyPassword-Generator.py
@@ -1,30 +1,31 @@
-#Password Generator Project
-from random import randint, shuffle;
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+# Password Generator Project
+from random import randint, shuffle, choice
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+           'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 password = []
 
-while nr_letters>0:
-    password.append(letters[randint(0, len(letters) -1)]);
-    nr_letters-=1
+while nr_letters > 0:
+    password.append(choice(letters))
+    nr_letters -= 1
 
-while nr_symbols>0:
-    password.append(symbols[randint(0, len(symbols) -1)]);
-    nr_symbols-=1
+while nr_symbols > 0:
+    password.append(choice(symbols))
+    nr_symbols -=1
 
-while nr_numbers>0:
-    password.append(numbers[randint(0, len(numbers) -1)]);
-    nr_numbers-=1
-shuffle(password);
+while nr_numbers > 0:
+    password.append(choice(numbers))
+    nr_numbers -= 1
+shuffle(password)
 passwordS = ""
 for pas in password:
-    passwordS += pas;
-    
+    passwordS += pas
+
 print(passwordS)
